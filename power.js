14ee08d4d0207ec1ad57cb61a59802d724a545ef
adambenkhader04-dev <adambenkhader04@gmail.com>
2025-09-26 20:26:41 -0400

    Delete power.js

--- power.js
+++
@@ -1,8 +0,0 @@
-export default function Power() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Power Rankings</h1>
-      <div className="card p-4">Power Rankings page placeholder (will compute rankings based on projections & recent form).</div>
-    </div>
-  )
-}

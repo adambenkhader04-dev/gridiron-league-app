bff7b5824913f15a7a8d8a2649f5a1487f61e1f3
adambenkhader04-dev <adambenkhader04@gmail.com>
2025-09-26 20:26:34 -0400

    Delete playoff.js

--- playoff.js
+++
@@ -1,8 +0,0 @@
-export default function Playoff() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Playoff Picture</h1>
-      <div className="card p-4">Playoff page placeholder (MVP excludes Monte Carlo sim per request).</div>
-    </div>
-  )
-}
